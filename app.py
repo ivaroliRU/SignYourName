3723f6de164de63c62ac6ac25f5fe2785e87b0af
Ivar Óli <ivartheoli@gmail.com>
2018-12-13 21:18:53 +0000

    post request from site to server

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -6,6 +6,12 @@ app.debug = True
 @app.route('/')
 def front_page():
     return render_template("index.html")
+
+@app.route('/add_msg', methods=['POST'])
+def add_msg():
+    data = request.get_json(silent=True)
+    print("Post: " + str(data))
+    return "typpi"
 
 if __name__ == '__main__':
     app.run(port=3000)
--- static/main.js
+++ static/main.js
@@ -13,6 +13,23 @@ $(window).scroll(function () {
     console.log("scrolling");
     if ($(window).scrollTop() + $(window).height() == $(document).height()) {
         console.log("typpi");
-        
+
     }
+});
+
+$('#submit-btn').click(function () {
+    console.log("asdf");
+    var _name = $("#name").val();
+    var _msg = $("#msg").val();
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: "/add_msg",
+        data: JSON.stringify({ name: _name, msg: _msg }),
+        success: function (data) {
+            console.log("Success!")
+        },
+        dataType: "json"
+    });
 });
